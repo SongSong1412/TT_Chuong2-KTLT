811f3b52e0f0410f8bb66cbd642659a191c4d4b3
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-08-01 22:07:33 +0700

    finish Bai8.1_Chuong2_Nhap xuat danh sach sinh vien

---
+++ Chuong2_KTLT_P2/Chuong2_KTLT/Bai8.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char MaMon[20];
+	char TenMon[50];
+	int SoTinChi;
+	float Diem;
+} MonHoc;
+
+typedef struct {
+	char MaSV[20];
+	char HoTen[50];
+	MonHoc MonHoc[5];
+	float DiemTB;
+} SinhVien;
+
+void NhapMonHoc(MonHoc *mon) {
+	printf("Nhap ma mon: ");
+	scanf("%s", mon->MaMon);
+	printf("Nhap ten mon: ");
+	scanf(" %[^\n]", mon->TenMon);
+	printf("Nhap so tin chi: ");
+	scanf("%d", &mon->SoTinChi);
+	printf("Nhap diem: ");
+	scanf("%f", &mon->Diem);
+}
+
+void NhapSinhVien(SinhVien *sv) {
+	printf("Nhap ma sinh vien: ");
+	scanf("%s", sv->MaSV);
+	printf("Nhap ho ten sinh vien: ");
+	scanf(" %[^\n]", sv->HoTen);
+	float tongDiem = 0;
+	int tongTinChi = 0;
+	for (int i = 0; i < 5; i++) {
+		printf("Nhap thong tin mon hoc thu %d:\n", i + 1);
+		NhapMonHoc(&sv->MonHoc[i]);
+		tongDiem += sv->MonHoc[i].Diem * sv->MonHoc[i].SoTinChi;
+		tongTinChi += sv->MonHoc[i].SoTinChi;
+	}
+	sv->DiemTB = tongDiem / tongTinChi;
+}
+
+void XuatMonHoc(MonHoc mon) {
+	printf("%s\t%s\t%d\t%.2f\n", mon.MaMon, mon.TenMon, mon.SoTinChi, mon.Diem);
+}
+
+void XuatSinhVien(SinhVien sv) {
+	printf("%s\t%s\t%.2f\n", sv.MaSV, sv.HoTen, sv.DiemTB);
+	for (int i = 0; i < 5; i++) {
+		XuatMonHoc(sv.MonHoc[i]);
+	}
+}
+
+void NhapDanhSachSinhVien(SinhVien sv[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("Nhap thong tin sinh vien thu %d:\n", i + 1);
+		NhapSinhVien(&sv[i]);
+	}
+}
+
+void XuatDanhSachSinhVien(SinhVien sv[], int n) {
+	printf("MSSV\tHo Ten\tDiem TB\n");
+	for (int i = 0; i < n; i++) {
+		XuatSinhVien(sv[i]);
+	}
+}
+
+int main() {
+	SinhVien sv[50];
+	int n;
+
+	printf("Nhap so luong sinh vien: ");
+	scanf("%d", &n);
+
+	NhapDanhSachSinhVien(sv, n);
+
+	printf("Danh sach sinh vien:\n");
+	XuatDanhSachSinhVien(sv, n);
+
+	return 0;
+}
