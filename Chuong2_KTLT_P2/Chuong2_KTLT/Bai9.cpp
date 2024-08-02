1830567d7850a5fde2cb7cc50f56efce184fb72d
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-08-02 22:39:03 +0700

    finish Bai9.1_Chuong2_Tong cac phan tu co chu so le

---
+++ Chuong2_KTLT_P2/Chuong2_KTLT/Bai9.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+bool SoHoanThien(int num) {
+	int tong = 1;
+	for (int i = 2; i <= sqrt((double)num); i++) {
+		if (num % i == 0) {
+			if (i == (num / i)) {
+				tong += i;
+			}
+			else {
+				tong+= i + (num / i);
+			}
+		}
+	}
+	return (tong== num && num != 1);
+}
+
+bool SoNguyenTo(int num) {
+	if (num <= 1) {
+		return false;
+	}
+	for (int i = 2; i <= (int)sqrt((double)num); i++) {
+		if (num % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void TaoMangNgauNhien(int a[][100], int m, int n) {
+	srand(time(NULL));
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			a[i][j] = rand() % 100; 
+		}
+	}
+}
+
+int TongChuSoLe(int a[][100], int m, int n) {
+	int tong = 0;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			int firstDigit = abs(a[i][j]);
+			while (firstDigit >= 10) {
+				firstDigit /= 10;
+			}
+			if (firstDigit % 2 != 0) {
+				tong += a[i][j];
+			}
+		}
+	}
+	return tong;
+}
+int main() {
+	int a[100][100];
+	int m, n;
+
+	printf("Nhap so hang m: ");
+	scanf("%d", &m);
+	printf("Nhap so cot n: ");
+	scanf("%d", &n);
+
+	TaoMangNgauNhien(a, m, n);
+
+	printf("Ma tran ngau nhien sau khi tao:\n");
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("Tong cac phan tu co chu so dau la chu so le: %d\n", TongChuSoLe(a, m, n));
+	return 0;
+}
